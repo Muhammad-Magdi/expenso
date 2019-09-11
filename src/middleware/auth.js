13ebeb31f376e73921f9a459faa3a765dec6643f
Muhammad Magdi <Muhammad.Magdi1024@gmail.com>
2019-09-12 00:11:57 +0200

    use http_status package

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,25 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const status = require('http-status');
 
 module.exports = async function(req, res, next) {
   let token = req.header('Authorization');
   if (!token) {
-    return res.status(400).send('Token is required!');
+    return res.status(status.BAD_REQUEST).send('Token is required!');
   }
   token = token.replace('Bearer ', '');
   jwt.verify(token, process.env.JWT_SECRET, async function(err, {_id}) {
     if (err) {
-      return res.status(401).json(err.message);
+      return res.status(status.UNAUTHORIZED).send(err.message);
     }
     try {
       req.user = await User.findById(_id);
       if (!req.user) {
-        return res.status(401).send('Invalid token!');
+        return res.status(status.UNAUTHORIZED).send('Invalid token!');
       }
       next();
     } catch (e) {
-      return res.status(500).json(e.message);
+      return res.status(status.INTERNAL_SERVER_ERROR).send(e.message);
     }
   });
 };
